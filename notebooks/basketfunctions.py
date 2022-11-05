3870e450108037af4897998668db43c414c10ea8
drahcir21 <richardgoh.2019@business.smu.edu.sg>
2022-11-05 18:33:42 +0800

    added basket analysis demo

---
+++ notebooks/basketfunctions.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import re
+
+def change_set(x):
+    x = str(x)
+    tmp = re.split('{|}', x)
+    tmp_set = [code.strip() for code in tmp[1].split(',')]
+    final_set = []
+    for code in tmp_set:
+        code = re.sub("[^0-9]", "", code)
+        final_set.append(code)
+    final_set = [int(code) for code in final_set]
+    return final_set
+
+def get_recent_SKUs(customerCode: str, sample_df: pd.DataFrame):
+    cust_df = sample_df[sample_df['CustomerCode'] == customerCode]
+    custSKUs = cust_df['ProductCode'].unique().tolist()
+    return custSKUs
+
+def get_product_history(customerCode: str, sample_df: pd.DataFrame):
+    cust_df = sample_df[sample_df['CustomerCode'] == customerCode]
+    product_qty = cust_df.groupby(['ProductCode'])['BaseQty'].sum()
+    product_amt = cust_df.groupby(['ProductCode'])['NetAmount'].sum()
+    return (product_qty, product_amt)
+
+def get_recommendations(custSKUs: list, top_x: int, df: pd.DataFrame) -> dict:
+    # Initialize empty list to store recommended products
+    recommendations = {}
+    i = 0
+    
+    while len(recommendations) < top_x and i < df.shape[0]:
+        antecedents = df.iloc[i].antecedents
+        consequents = df.iloc[i].consequents
+        confidence = df.iloc[i].confidence
+        
+        if set(antecedents).issubset(set(custSKUs)) and not set(consequents).issubset(set(custSKUs)):
+            for code in consequents:
+                if code not in recommendations.keys():
+                    recommendations[code] = confidence
+                else:
+                    recommendations[code] += confidence
+        i += 1
+    
+    return recommendations
